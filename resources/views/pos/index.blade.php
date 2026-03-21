6c08fcffdd1a41b8e8dd3ead7793adba86699089
zoeyestrella2025-netizen <zoeyestrella2025@gmail.com>
2026-03-21 00:55:48 -0500

    Add compiled assets for production

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demostración - AutoCars Cabrera</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Alpine.js -->
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <!-- Google Fonts: Space Grotesk & Inter -->
